1694b9392bfd085a083c845267f316043a72e918
prajwal zingare <prajwalzingre@gmail.com>
2022-06-27 10:08:03 +0530

    prajwal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import Tempapp from './Components/Tempapp';
-
+import "./App.css";
+import Tempapp from "./Components/Tempapp";
 
 function App() {
   return (
-<>
-<Tempapp/>
-
-</>
+    <>
+      <Tempapp />
+    </>
   );
 }
 
--- src/Components/Tempapp.js
+++ src/Components/Tempapp.js
@@ -1,71 +1,64 @@
-import React, { useEffect, useState } from 'react'
-import "./css/style.css"
-
+import React, { useEffect, useState } from "react";
+import "./css/style.css";
 
 const Tempapp = () => {
-    const [city, setcity] = useState(null);
+  const [city, setcity] = useState(null);
 
+  // for user what exactly typing
+  const [search, setsearch] = useState("mumbai");
 
-    // for user what exactly typing 
-    const [search, setsearch ] = useState('mumbai')
-
-    useEffect(()=>{
-      const fetchapi = async()=>{
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=4cd6e65c93931133bb70a0a36b4828c0` 
+  useEffect(() => {
+    const fetchapi = async () => {
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=0c998cb040f1a46e4a3d01b7136bf963`;
       const Inprocess = await fetch(url);
       const Inprocces2 = await Inprocess.json();
       // console.log(Inprocces2)
-      setcity(Inprocces2.main)
-
-      }
-      fetchapi();
-    },[search])
+      setcity(Inprocces2.main);
+    };
+    fetchapi();
+  }, [search]);
 
   return (
-<>
-    <div className='box'>
-        <div className='inputData'>
-               <div className='inputFeild'>
-               <input
-         type="search"
-         placeholder='Enter Name'
-         onChange={(events)=>{setsearch(events.target.value)}}
-         defaultValue = "mumbai"
-         className='inputone'
-       
-          />
-               </div>
-       
+    <>
+      <div className="box">
+        <div className="inputData">
+          <div className="inputFeild">
+            <input
+              type="search"
+              placeholder="Enter Name"
+              onChange={(events) => {
+                setsearch(events.target.value);
+              }}
+              defaultValue="mumbai"
+              className="inputone"
+            />
+          </div>
         </div>
-       {/* ternary operator */}
-       {/* agar city nai milti to data not found
+        {/* ternary operator */}
+        {/* agar city nai milti to data not found
        aur milti hai to ye */}
-        {
-          !city ? (<p> Data Not found</p>) : (
+        {!city ? (
+          <p> Data Not found</p>
+        ) : (
           <>
-            <div className='info'>
-            <h2 className='location'>
-            <i className="fa-solid fa-street-view"></i> {search}
-            </h2>
-            <h1 className='temp'>
-              {city.temp} °Cel
-            </h1>
-            <h3 className='tempmin_max'>Min :{city.temp_min}°Cel | Max : {city.temp_max}°Cel </h3>
-         </div>
+            <div className="info">
+              <h2 className="location">
+                <i className="fa-solid fa-street-view"></i> {search}
+              </h2>
+              <h1 className="temp">{city.temp} °Cel</h1>
+              <h3 className="tempmin_max">
+                Min :{city.temp_min}°Cel | Max : {city.temp_max}°Cel{" "}
+              </h3>
+            </div>
 
-         <div className='wave -one'></div>
-         <div className='wave -two'></div>
-         <div className='wave -three'></div>
-         </>
-          )
-        }
-        
-        
+            <div className="wave -one"></div>
+            <div className="wave -two"></div>
+            <div className="wave -three"></div>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
 
-
-    </div>
-</>
-  )
-}
-
-export default Tempapp
+export default Tempapp;
